b62aa708c21f633350270f5b023233a3b63e7569
Romuald Brillout <github.com@brillout.com>
2020-12-11 11:55:27 +0100

    use console.error on browser-side

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 export { assert };
 export { assertWarning };
 
-const prefix = "[@brillout/assert] ";
+const errorName = "[@brillout/assert] Assertion Failure.";
 
 function assert(condition: unknown, ...msgs: unknown[]): asserts condition {
   if (condition) {
     return;
   }
 
-  const errorMessage = createErrorMessage(msgs);
-
-  throw new Error(errorMessage);
+  if (isNodejs()) {
+    const errorMessage = createErrorMessage(msgs);
+    throw new Error(errorMessage);
+  } else {
+    if (msgs.length > 0) {
+      console.error(...msgs);
+    }
+    throw new Error(errorName);
+  }
 }
 
 function assertWarning(condition: unknown, ...msgs: unknown[]): void {
@@ -18,20 +24,24 @@ function assertWarning(condition: unknown, ...msgs: unknown[]): void {
     return;
   }
 
-  const errorMessage = createErrorMessage(msgs);
-
-  console.error(new Error(errorMessage));
+  if (isNodejs()) {
+    const errorMessage = createErrorMessage(msgs);
+    console.error(new Error(errorMessage));
+  } else {
+    if (msgs.length > 0) {
+      console.error(...msgs);
+    }
+    console.error(new Error(errorName));
+  }
 }
 
 function createErrorMessage(msgs: unknown[]): string {
-  let errorMessage = [`${prefix}Assertion failed.`, ...msgs.map(log)].join(
-    "\n\n"
-  );
+  let errorMessage = [errorName, ...msgs.map(stringify)].join("\n\n");
 
   return errorMessage;
 }
 
-function log(thing: unknown): void {
+function stringify(thing: unknown): void {
   const inspectOptions = {
     depth: Infinity,
     showHidden: true,
@@ -40,11 +50,7 @@ function log(thing: unknown): void {
     getters: true,
   };
 
-  if (isNodejs()) {
-    return eval("require")("util").inspect(thing, inspectOptions);
-  } else {
-    return require("browser-util-inspect")(thing, inspectOptions);
-  }
+  return eval("require")("util").inspect(thing, inspectOptions);
 }
 
 function isNodejs(): boolean {
